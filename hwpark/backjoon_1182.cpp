e9a455ad19bbdb39b707e6444bdbc9422141f835
hw0404 <guddn1334@icloud.com>
2022-09-22 01:15:16 +0900

    edited code

--- hwpark/backjoon_1182.cpp
+++ hwpark/backjoon_1182.cpp
@@ -2,19 +2,14 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
-#include<cstring>
-#define MAX_N 21
-
+#define MAX_N 41
 using namespace std;
 
-int N,M;
+int N,M,sum,total;
 int result[MAX_N];
-int visited[MAX_N];
 int num[MAX_N];
 vector<int> v;
-int cycle;
-int sum;
-int total;
+
 void input(){
     cin >> N >> M;
     for(int i=0;i<N;i++){
@@ -25,19 +20,14 @@ void input(){
     }
     sort(v.begin(),v.end());
 }
-
 void solve( int cnt ){
-    if(cnt == cycle ){
-      //  for(int i=0;i<cnt;i++) cout<<num[i]<<" " ;
-        if(sum == M){
-             total+=1;
-          }
+    if(cnt == N ){
         return ;
     }
     for(int i=0;i<v.size();i++){
-        if(visited[i]==0){
             if(cnt>0){
                 if(num[cnt-1]<i){
+                    if(sum+v[i]==M) total++;
                     num[cnt]=i;
                     sum+=v[i];
                     solve(cnt+1);
@@ -45,28 +35,20 @@ void solve( int cnt ){
                 }
             }
             else{
-                sum+=v[i];
+                if(sum+v[i]==M) total++;
                 num[cnt]=i;
+                sum+=v[i];
                 solve(cnt+1);
                 sum-=v[i];
             }
         }
-    }
 }
 void solution(){
     input();
-    for(int i=1;i<=v.size();i++){
-        cycle=i;
-        sum=0;
-        memset(visited,0,sizeof(visited));
-        solve(0);
-
-    }
+    solve(0);
     cout<<total<<"\n";
-    
 }
 int main(){
     solution();
 return 0;
 }
-
